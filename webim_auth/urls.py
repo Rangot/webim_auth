fc12f703dc87913d27b7346e1f691692e01a2fe9
Rangot <arty-a.2017@mail.ru>
2019-01-23 13:24:54 +0300

    logout added

--- webim_auth/urls.py
+++ webim_auth/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from oauth.views import *
+from django.contrib.auth.views import logout_then_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('social_django.urls', namespace='social')),
     path('', index, name='index'),
+    path('', include('social_django.urls', namespace='social')),
+    path('logout/', logout_then_login, name='logout'),
 ]
